a8dadaec64400598e9bea6d2e61caabd4f05ee8e
ACMMM2020-MTL <60142685+ACMMM2020-MTL@users.noreply.github.com>
2020-04-21 15:46:30 +0800

    Update AFA.py

--- AFA.py
+++ AFA.py
@@ -51,12 +51,16 @@ class SAM(nn.Module):
     def __init__(self):
         super(SAM, self).__init__()
 
-        self.conv_AA = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7, stride=1, padding=3)
-        self.conv_AB = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7, stride=1, padding=3)
-        self.conv_BA = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7, stride=1, padding=3)
-        self.conv_BB = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7, stride=1, padding=3)
+        self.conv_A = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=1, stride=1, padding=0)
+        self.fc_AA = nn.Linear(in_features=361, out_features=361)
+        self.fc_AB = nn.Linear(in_features=361, out_features=361)
+        self.conv_B = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=1, stride=1, padding=0)
+        self.fc_BA = nn.Linear(in_features=361, out_features=361)
+        self.fc_BB = nn.Linear(in_features=361, out_features=361)
 
         self.relu = nn.ReLU(inplace=True)
+        self.bn1 = nn.BatchNorm2d(1)
+        self.bn2 = nn.BatchNorm2d(1)
 
     def forward(self, x1, x2):
         b, c, height, width = x1.size()
@@ -65,18 +69,22 @@ class SAM(nn.Module):
         d_B = torch.mean(original_x2, dim=1, keepdim=True)
         d_concat = torch.cat((d_A, d_B), dim=1)
 
-        g_AA = self.conv_AA(d_concat).view(b, -1, height * width)
-        g_AB = self.conv_AB(d_concat).view(b, -1, height * width)
-        g = torch.cat((g_AA, g_AB), dim=1)
-        w = F.softmax(g, dim=1).permute(1, 0, 2)
+        g_A = self.relu(self.bn1(self.conv_A(d_concat)))
+        g_A_vector = g_A.view(b, -1)
+        w_AA = torch.unsqueeze(self.relu(self.fc_AA(g_A_vector)), dim=1)
+        w_AB = torch.unsqueeze(self.relu(self.fc_AB(g_A_vector)), dim=1)
+        w = torch.cat((w_AA, w_AB), dim=1)
+        w = F.softmax(w, dim=1).permute(1, 0, 2)
         w_AA = w[0].view(w[0].size(0), -1, height, width)
         w_AB = w[1].view(w[1].size(0), -1, height, width)
         out1 = x1 + w_AA * original_x1 + w_AB * original_x2 - original_x1
 
-        g_BA = self.conv_BA(d_concat).view(b, -1, height * width)
-        g_BB = self.conv_BB(d_concat).view(b, -1, height * width)
-        g = torch.cat((g_BA, g_BB), dim=1)
-        w = F.softmax(g, dim=1).permute(1, 0, 2)
+        g_B = self.relu(self.bn2(self.conv_B(d_concat)))
+        g_B_vector = g_B.view(b, -1)
+        w_BA = torch.unsqueeze(self.relu(self.fc_BA(g_B_vector)), dim=1)
+        w_BB = torch.unsqueeze(self.relu(self.fc_BB(g_B_vector)), dim=1)
+        w = torch.cat((w_BA, w_BB), dim=1)
+        w = F.softmax(w, dim=1).permute(1, 0, 2)
         w_BA = w[0].view(w[0].size(0), -1, height, width)
         w_BB = w[1].view(w[1].size(0), -1, height, width)
         out2 = x2 + w_BA * original_x1 + w_BB * original_x2 - original_x2
